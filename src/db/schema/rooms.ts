e38f20e7bd7c38b2ecd90b5522da238a5da65df1
Juliano Cardoso <ocardosojuliano@gmail.com>
2025-07-11 09:13:56 -0300

    feat: Add new routes

--- src/db/schema/rooms.ts
+++ src/db/schema/rooms.ts
@@ -1,9 +1,9 @@
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const rooms = pgTable("rooms", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  name: text("name").notNull(),
-  description: text("description"),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  id: uuid().primaryKey().defaultRandom(),
+  name: text().notNull(),
+  description: text(),
+  createdAt: timestamp().notNull().defaultNow(),
+  updatedAt: timestamp().notNull().defaultNow(),
 });
